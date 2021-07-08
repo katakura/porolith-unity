443e51e865f24f2acd9efc9da86773d724760c65
Yoshimasa Katakura <yoshiamsa.katakura@outlook.com>
2021-07-08 14:03:18 +0900

    とりあえずブロックを落とす
    こちらの記事を参考に勉強中
    https://masavlog.com/category/programming/tetris/

---
+++ Assets/Mino.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mino : MonoBehaviour
+{
+    public float previousTime;
+    // minoの落ちる時間
+    public float fallTime = 1f;
+
+    // ステージの大きさ
+    private static int width = 15;
+    private static int height = 25;
+
+    // mino回転
+    public Vector3 rotationPoint;
+
+    // 今回の追加
+    private static Transform[,] grid = new Transform[width, height];
+
+    void Update()
+    {
+        MinoMovememt();
+    }
+
+    private void MinoMovememt()
+    {
+        // 左矢印キーで左に動く
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            
+            if (!ValidMovement()) 
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
+
+        }
+        // 右矢印キーで右に動く
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            
+            if (!ValidMovement()) 
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+        // 自動で下に移動させつつ、下矢印キーでも移動する
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - previousTime >= fallTime) 
+        {
+            transform.position += new Vector3(0, -1, 0);
+            
+            if (!ValidMovement()) 
+            {
+                transform.position -= new Vector3(0, -1, 0);
+                // 今回の追加
+                AddToGrid();
+                this.enabled = false;
+                FindObjectOfType<SpawnMino>().NewMino();
+            }
+
+            previousTime = Time.time;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // ブロックの回転
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+        }
+    }
+
+    // 今回の追加
+    void AddToGrid() 
+    {
+        foreach (Transform children in transform) 
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundX, roundY] = children;
+        }
+    }
+
+    
+    // minoの移動範囲の制御
+    bool ValidMovement()
+    {
+
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            // minoがステージよりはみ出さないように制御
+            if (roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
+            {
+                return false;
+            }
+            // 今回の追加
+            if (grid[roundX, roundY] != null)
+            {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
